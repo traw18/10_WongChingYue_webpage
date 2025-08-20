b72982a0a758c0df3969c5cc56f368d325c25b57
traw18 <traw18@gmail.com>
2025-08-20 12:52:42 +0800

    Aug 20

--- js/solo.js
+++
@@ -1,8 +0,0 @@
-// script.js
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    hamburger.classList.toggle('active');
-});
